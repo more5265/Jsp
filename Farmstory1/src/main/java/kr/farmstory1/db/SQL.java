9c5b226e78dedb5d07a0d9b9aaf3b1c1b7e78624
more5265 <more5265@gmail.com>
2023-08-21 17:30:17 +0900

    8.21

--- Farmstory1/src/main/java/kr/farmstory1/db/SQL.java
+++ Farmstory1/src/main/java/kr/farmstory1/db/SQL.java
@@ -68,5 +68,20 @@ public class SQL {
 	public final static String DELETE_ARTICLE = "DELETE FROM `Article` WHERE `no`=? OR `parent`=?";
 	public final static String DELETE_COMMENT = "DELETE FROM `Article` WHERE `no`=?";
 	
+	// Product
+	
+	public final static String INSERT_PRODUCT = "INSERT INTO `Product` SET"
+												+ "`type`=?,"
+												+ "`pName`=?,"
+												+ "`price`=?,"
+												+ "`delivery`=?,"
+												+ "`stock`=?,"
+												+ "`thumb1`=?,"
+												+ "`thumb2`=?,"
+												+ "`thumb3`=?,"
+												+ "`seller`=?,"
+												+ "`etc`=?,"
+												+ "`rdate`=NOW()";
+												
 	
 }
